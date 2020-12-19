9a9a45bee5d2ef61f5e2a56fd7e6fbe484bb8d30
MURAOKA Taro <koron.kaoriya@gmail.com>
2020-12-19 12:41:54 +0900

    configure kaoriya keymap features

--- keyboards/crkbd/keymaps/kaoriya/rules.mk
+++ keyboards/crkbd/keymaps/kaoriya/rules.mk
@@ -1,1 +1,11 @@
-OLED_DRIVER_ENABLE  = yes
+# Enable OLED
+OLED_DRIVER_ENABLE = yes
+
+# NO mouse keys
+MOUSEKEY_ENABLE = no
+
+# Enable WS2812 RGB underlight
+RGBLIGHT_ENABLE = yes
+
+# Link-Time-Optimization: for small firmware
+LTO_ENABLE = yes
