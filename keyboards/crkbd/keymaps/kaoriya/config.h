0ad5ab688817c430983816262fc04cc86ea828a7
MURAOKA Taro <koron.kaoriya@gmail.com>
2020-12-19 19:27:37 +0900

    enable RGB matrix

--- keyboards/crkbd/keymaps/kaoriya/config.h
+++ keyboards/crkbd/keymaps/kaoriya/config.h
@@ -21,13 +21,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-//#define USE_MATRIX_I2C
-
-/* Select hand configuration */
-
+// Hand configuration: left is master always
 #define MASTER_LEFT
-// #define MASTER_RIGHT
-// #define EE_HANDS
 
 #define USE_SERIAL_PD2
 
@@ -45,3 +40,58 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 #define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
+
+#ifdef RGB_MATRIX_ENABLE
+//#   define RGB_DISABLE_WHEN_USB_SUSPENDED true
+//#   define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
+//#   define RGB_MATRIX_LED_PROCESS_LIMIT (DRIVER_LED_TOTAL + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
+//#   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+
+#   define RGB_MATRIX_KEYPRESSES
+#   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#   define RGB_MATRIX_MAXIMUM_BRIGHTNESS 120
+#   define RGB_MATRIX_HUE_STEP 8
+#   define RGB_MATRIX_SAT_STEP 8
+#   define RGB_MATRIX_VAL_STEP 5
+#   define RGB_MATRIX_SPD_STEP 10
+
+// Enabled matrix effects:
+//  - RGB_MATRIX_SOLID_COLOR (*)
+//  - RGB_MATRIX_BREATHING (*)
+//  - RGB_MATRIX_CYCLE_ALL
+//  - RGB_MATRIX_CYCLE_LEFT_RIGHT (*)
+//  - RGB_MATRIX_CYCLE_PINWHEEL (*)
+//  - RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+//  - RGB_MATRIX_DUAL_BEACON (?)
+//  - RGB_MATRIX_TYPING_HEATMAP
+//  - RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+//  - RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+//  - RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+//  - RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+//  - RGB_MATRIX_MULTISPLASH (?)
+//  - RGB_MATRIX_SOLID_MULTISPLASH
+#   define DISABLE_RGB_MATRIX_ALPHAS_MODS
+#   define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+#   define DISABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+#   define DISABLE_RGB_MATRIX_BAND_SAT
+#   define DISABLE_RGB_MATRIX_BAND_VAL
+#   define DISABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
+#   define DISABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
+#   define DISABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+#   define DISABLE_RGB_MATRIX_BAND_SPIRAL_VAL
+#   define DISABLE_RGB_MATRIX_CYCLE_UP_DOWN
+#   define DISABLE_RGB_MATRIX_CYCLE_OUT_IN
+#   define DISABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+#   define DISABLE_RGB_MATRIX_CYCLE_SPIRAL
+#   define DISABLE_RGB_MATRIX_RAINBOW_BEACON
+#   define DISABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+#   define DISABLE_RGB_MATRIX_RAINDROPS
+#   define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+#   define DISABLE_RGB_MATRIX_DIGITAL_RAIN
+#   define DISABLE_RGB_MATRIX_SOLID_REACTIVE
+#   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+#   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+#   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+#   define DISABLE_RGB_MATRIX_SPLASH
+#   define DISABLE_RGB_MATRIX_SOLID_SPLASH
+#endif
