04ab982624e728c2564b7905c74ae5f9705baa28
MURAOKA Taro <koron.kaoriya@gmail.com>
2020-12-19 17:54:11 +0900

    make list_keyboards.sh cache-able its result
    for speed.

--- util/list_keyboards.sh
+++ util/list_keyboards.sh
@@ -3,6 +3,11 @@
 #
 # This allows us to exclude keyboards by including a .noci file.
 
+if [ -e .keyboards_cache ] ; then
+	cat .keyboards_cache
+	exit 0
+fi
+
 find -L keyboards -type f -name rules.mk | grep -v keymaps | sed 's!keyboards/\(.*\)/rules.mk!\1!' | while read keyboard; do
 	[ "$1" = "noci" -a -e "keyboards/${keyboard}/.noci" ] || echo "$keyboard"
 done
