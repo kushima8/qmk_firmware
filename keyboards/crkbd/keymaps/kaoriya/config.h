db8a8a15fc0b17d337289e2108e03213fcd43864
MURAOKA Taro <koron.kaoriya@gmail.com>
2020-12-20 10:55:42 +0900

    remove unsed macro. resurrect RGB matrix

--- keyboards/crkbd/keymaps/kaoriya/config.h
+++ keyboards/crkbd/keymaps/kaoriya/config.h
@@ -24,8 +24,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Hand configuration: left is master always
 #define MASTER_LEFT
 
-#define USE_SERIAL_PD2
-
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 100
 
@@ -44,8 +42,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifdef RGB_MATRIX_ENABLE
 //#   define RGB_DISABLE_WHEN_USB_SUSPENDED true
 //#   define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
-//#   define RGB_MATRIX_LED_PROCESS_LIMIT (DRIVER_LED_TOTAL + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
-//#   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
 
 #   define RGB_MATRIX_KEYPRESSES
 #   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
@@ -54,6 +50,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #   define RGB_MATRIX_SAT_STEP 8
 #   define RGB_MATRIX_VAL_STEP 5
 #   define RGB_MATRIX_SPD_STEP 10
+
+// Limitation on LED number: increases keyboard responsiveness
+#   define RGB_MATRIX_LED_PROCESS_LIMIT ((DRIVER_LED_TOTAL + 4) / 5)
+#   define RGB_MATRIX_LED_FLUSH_LIMIT   16
 
 // Enabled matrix effects:
 //  - RGB_MATRIX_SOLID_COLOR (*)
