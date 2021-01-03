fe86b13858161234f7b058ae3c0308bf6f8aee31
MURAOKA Taro <koron.kaoriya@gmail.com>
2021-01-04 00:33:51 +0900

    crkbd:kaoriya 001 release
    - optimize RGB LED params
    - format keymap.json
    - add +KaoriYa remark on logo

--- keyboards/crkbd/keymaps/kaoriya/config.h
+++ keyboards/crkbd/keymaps/kaoriya/config.h
@@ -44,9 +44,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define RGBLIGHT_ANIMATIONS
 #    define RGBLED_NUM 27
 #    define RGBLIGHT_LIMIT_VAL 120
-#    define RGBLIGHT_HUE_STEP 10
+#    define RGBLIGHT_HUE_STEP 17
 #    define RGBLIGHT_SAT_STEP 17
-#    define RGBLIGHT_VAL_STEP 17
+#    define RGBLIGHT_VAL_STEP 12
 #endif
 
 #define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
