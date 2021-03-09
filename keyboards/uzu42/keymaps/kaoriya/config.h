e0f016726a22dd2c80c47588b60883e4852caf5c
MURAOKA Taro <koron.kaoriya@gmail.com>
2021-03-10 00:37:47 +0900

    uzu42: kaoriya keymap first commit

--- keyboards/uzu42/keymaps/kaoriya/config.h
+++ keyboards/uzu42/keymaps/kaoriya/config.h
@@ -1,24 +1,32 @@
 /*
-This is the c configuration file for the keymap
-
-Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
+Copyright 2021 MURAOKA Taro (aka KoRoN, @kaoriya)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
 (at your option) any later version.
-
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
+
+#define KAORIYA_FIRMWARE_VERSION r1
+
+#undef PRODUCT
+#define PRODUCT uzu42 +KaoriYa/KAORIYA_FIRMWARE_VERSION
+
+// Minimal layer state for small binary
+#define LAYER_STATE_8BIT
+
+// Detect hand configuration (left or right) by matrix.
+#define SPLIT_HAND_MATRIX_GRID D7, B3
+#define SPLIT_HAND_MATRIX_GRID_LOW_IS_RIGHT
+#define MATRIX_MASKED
 
 // Detect master/slave configuration by USB instead of VBUS.
 #define SPLIT_USB_DETECT
@@ -30,10 +38,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 54
-#define RGBLIGHT_LIMIT_VAL 100
+#define RGBLIGHT_LIMIT_VAL 120
 #define RGBLIGHT_HUE_STEP 17
 #define RGBLIGHT_SAT_STEP 17
-#define RGBLIGHT_VAL_STEP 10
+#define RGBLIGHT_VAL_STEP 12
 
-// Use the lily version to get the uzu42 logo instead of the qmk logo
+// Disable some QMK Features as default.
+#define NO_ACTION_ONESHOT
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+
 #define OLED_FONT_H "keymaps/default/glcdfont_uzu42.c"
