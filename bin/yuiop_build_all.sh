fc5ea015ea3c00b69b2a41954481a813ee3601c1
MURAOKA Taro <koron.kaoriya@gmail.com>
2021-05-15 11:04:41 +0900

    add yuiop60hh4 to build test script

--- bin/yuiop_build_all.sh
+++ bin/yuiop_build_all.sh
@@ -5,6 +5,6 @@ set -eu
 make -j8 handwired/yuiop/yuiop27:default
 make -j8 handwired/yuiop/yuiop47:default
 make -j8 handwired/yuiop/yuiop50d:default
-make -j8 handwired/yuiop/yuiop50d:test
 make -j8 handwired/yuiop/yuiop22rs:default
 make -j8 handwired/yuiop/yuiop63hhs:default
+make -j8 handwired/yuiop/yuiop60hh4:default
