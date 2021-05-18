ce3d4615f216029aca5842e6125245a9bd1c48c6
utubo <utbutb@gmail.com>
2021-05-18 16:06:29 +0900

    rules.mkを追加、日本語記号の処理を別ファイルに分離

--- keyboards/re42/keymaps/utubo/keymap.c
+++ keyboards/re42/keymaps/utubo/keymap.c
@@ -16,6 +16,7 @@
  */
 #include QMK_KEYBOARD_H
 #include "keymap_jp.h"
+#include "keymap_jp_util.h"
 
 enum layer_number {
     _DEFAULT= 0,
@@ -228,42 +229,6 @@ static bool custom_lower(uint16_t keycode) {
     return false;
 }
 
-static bool process_jp_symbols(uint16_t keycode, bool pressed) {
-    if (!pressed) {
-        return true;
-    }
-    uint8_t shift = keyboard_report->mods & (MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT));
-    if (!shift) {
-        return true;
-    }
-    uint16_t s;
-    switch (keycode) {
-        // Replace Shift-Symbols like ANSI for JIS.
-        case JP_2:    s = JP_AT; break;
-        case JP_6:    s = JP_CIRC; break;
-        case JP_7:    s = JP_AMPR; break;
-        case JP_8:    s = JP_ASTR; break;
-        case JP_9:    s = JP_LPRN; break;
-        case JP_0:    s = JP_RPRN; break;
-        case JP_GRV:  s = JP_TILD; break;
-        case JP_EQL:  s = JP_PLUS; break;
-        case JP_MINS: s = JP_UNDS; break;
-        case JP_QUOT: s = JP_DQUO; break;
-        case JP_SCLN: s = JP_COLN; break;
-        default: return true;
-    }
-    unregister_mods(shift);
-    tap_code16(s);
-    register_mods(shift);
-    return false;
-}
-
-static void tap_code16jp(uint16_t keycode) {
-    if (process_jp_symbols(keycode, true)) {
-        tap_code16(keycode);
-    }
-}
-
 static bool sh_t16(uint16_t keycode) {
     swap_hands = !swap_hands;
     if (tap_state == SINGLE_TAP) {
@@ -298,7 +263,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case SH_JPQT: return sh_t16(JP_QUOT);
     }
 
-    return process_jp_symbols(keycode, record->event.pressed);
+    return process_jp_symbols(keycode, record);
 }
 
 #ifdef OLED_DRIVER_ENABLE
