242edb9298bc589174a13c8c6d0635e2fce7727f
utubo <utbutb@gmail.com>
2021-05-18 13:18:02 +0900

    rules.mkを追加、日本語記号の処理を別ファイルに分離

---
+++ keyboards/re42/keymaps/utubo/keymap_jp_util.h
@@ -0,0 +1,29 @@
+/* Copyright 2020 utubo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+/*
+ * SHIFT+JP_XXXをANSI配列のように対応させます。(対応させた場合、ホールドは適用されません)
+ * 以下のように実行してください。
+ *     bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+ *         return bool process_jp_symbols(keycode, record);
+ *     }
+ */
+bool process_jp_symbols(uint16_t keycode, keyrecord_t *record);
+
+/* tap_code16を上記に対応させたものです。 */
+void tap_code16jp(uint16_t keycode);
+
