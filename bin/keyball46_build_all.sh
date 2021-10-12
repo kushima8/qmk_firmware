b1ec2770aa6c53fdbe80bde01867f30fddf35216
MURAOKA Taro <koron.kaoriya@gmail.com>
2021-10-12 23:38:44 +0900

    keyball46: update build script

--- bin/keyball46_build_all.sh
+++ bin/keyball46_build_all.sh
@@ -2,12 +2,20 @@
 
 set -eu
 
-version=1.0.0
+version=1.1.0
 
-for keymap in default \
-  test test_Left test_Both \
-  via via_Left via_Both
-do
-  make -j8 keyball/rev1/ball:${keymap} keyball/rev1/noball:${keymap}
+keymaps=()
+keymaps+=(default)
+keymaps+=(test test_Left test_Both)
+keymaps+=(via via_Left via_Both)
+
+targets=()
+for keymap in "${keymaps[@]}" ; do
+  targets+=(keyball/rev1/ball:${keymap} keyball/rev1/noball:${keymap})
+done
+
+make -j8 SKIP_GIT=true "${targets[@]}"
+
+for keymap in "${keymaps[@]}" ; do
   zip -9 tmp/keyball_${keymap}_${version}.zip keyball_rev1_{ball,noball}_${keymap}.hex
 done
