db86b6ee7c97eafa85d04e13d96522939fe9ad5d
MURAOKA Taro <koron.kaoriya@gmail.com>
2021-10-12 23:19:21 +0900

    keyball46: disable OLED, enable RGBLIGHT

--- keyboards/keyball/keymaps/via_Left/config.h
+++ keyboards/keyball/keymaps/via_Left/config.h
@@ -29,6 +29,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define RGBLIGHT_HUE_STEP 10
 #    define RGBLIGHT_SAT_STEP 17
 #    define RGBLIGHT_VAL_STEP 17
+
+#    define RGBLIGHT_ANIMATIONS
 #endif
 
 #undef PRODUCT_ID
