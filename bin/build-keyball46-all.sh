cdd4509ed70f61f57925043ce5b045e21d7da7b4
MURAOKA Taro <koron.kaoriya@gmail.com>
2022-01-10 10:48:54 +0900

    rename build scripts

--- bin/build-keyball46-all.sh
+++ bin/build-keyball46-all.sh
@@ -14,7 +14,7 @@ for keymap in "${keymaps[@]}" ; do
   targets+=(keyball/rev1/ball:${keymap} keyball/rev1/noball:${keymap})
 done
 
-make -j8 SKIP_GIT=true "${targets[@]}"
+make -j8 SKIP_GIT=yes "${targets[@]}"
 
 for keymap in "${keymaps[@]}" ; do
   zip -9 tmp/keyball_${keymap}_${version}.zip keyball_rev1_{ball,noball}_${keymap}.hex
