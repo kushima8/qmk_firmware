2c81f42341619e7af412517586ba0a13ccaf0c55
MURAOKA Taro <koron.kaoriya@gmail.com>
2022-01-11 01:55:02 +0900

    keyball61: fix duplex matrix

--- keyboards/keyball61/matrix.c
+++ keyboards/keyball61/matrix.c
@@ -10,62 +10,57 @@
 static pin_t row_pins[PINNUM_ROW] = MATRIX_ROW_PINS;
 static pin_t col_pins[PINNUM_COL] = MATRIX_COL_PINS;
 
-void matrix_init_custom(void) {
-#if 0
-    split_pre_init();
-    split_post_init();
-#endif
+static inline void set_pin_input(pin_t pin) {
+    setPinInputHigh(pin);
 }
 
 static void set_pins_input(pin_t* pins, uint8_t n) {
     for (uint8_t i = 0; i < n; i++) {
-        setPinInputHigh(pins[i]);
+        set_pin_input(pins[i]);
     }
 }
 
-static void set_pins_output(pin_t* pins, uint8_t n) {
-    for (uint8_t i = 0; i < n; i++) {
-        setPinOutput(pins[i]);
-        writePinHigh(pins[i]);
-    }
+static inline void set_pin_output(pin_t pin) {
+    setPinOutput(pin);
+    writePinLow(pin);
+}
+
+static inline bool get_pin(pin_t pin) {
+    return readPin(pin);
 }
 
 static bool duplex_scan(matrix_row_t current_matrix[]) {
     bool changed = false;
 
     // scan column to row
-    set_pins_input(col_pins, PINNUM_COL);
-    set_pins_output(row_pins, PINNUM_ROW);
     for (uint8_t row = 0; row < PINNUM_ROW; row++) {
-        writePinLow(row_pins[row]);
+        set_pin_output(row_pins[row]);
         matrix_output_select_delay();
         matrix_row_t next = current_matrix[row];
         for (uint8_t col = 0; col < PINNUM_COL; col++) {
-            bool on = !readPin(col_pins[col]);
+            bool on = !get_pin(col_pins[col]);
             if (on) {
                 next |= 1 << col; 
             } else {
                 next &= ~(1 << col);
             }
         }
-        writePinHigh(row_pins[row]);
+        set_pin_input(row_pins[row]);
+        matrix_output_unselect_delay(row, next != 0);
         if (current_matrix[row] != next) {
             current_matrix[row] = next;
             changed = true;
         }
-        matrix_output_unselect_delay(row, next != 0);
     }
 
     // scan row to column.
-    set_pins_input(row_pins, PINNUM_ROW);
-    set_pins_output(col_pins, PINNUM_COL);
     for (uint8_t col = 0; col < PINNUM_COL; col++) {
         bool pressed = false;
-        writePinLow(col_pins[col]);
+        set_pin_output(col_pins[col]);
         matrix_output_select_delay();
         matrix_row_t shifter = ((matrix_row_t)1) << (col + PINNUM_COL);
         for (uint8_t row = 0; row < PINNUM_ROW; row++) {
-            bool on = !readPin(row_pins[row]);
+            bool on = !get_pin(row_pins[row]);
             matrix_row_t prev = current_matrix[row];
             if (on) {
                 current_matrix[row] |= shifter;
@@ -77,9 +72,10 @@ static bool duplex_scan(matrix_row_t current_matrix[]) {
                 changed = true;
             }
         }
-        writePinHigh(col_pins[col]);
+        set_pin_input(col_pins[col]);
         matrix_output_unselect_delay(col, pressed);
     }
+
     return changed;
 }
 
@@ -87,4 +83,11 @@ bool matrix_scan_custom(matrix_row_t current_matrix[]) {
     bool changed = duplex_scan(current_matrix);
     // TODO: consider split
     return changed;
+}
+
+void matrix_init_custom(void) {
+    //split_pre_init();
+    set_pins_input(col_pins, PINNUM_COL);
+    set_pins_input(row_pins, PINNUM_ROW);
+    //split_post_init();
 }
