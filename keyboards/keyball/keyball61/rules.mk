501af4d66755d442581dacec1a6926b733492670
MURAOKA Taro <koron.kaoriya@gmail.com>
2022-01-16 00:10:19 +0900

    keyball61: both sides can be primary keyboard

--- keyboards/keyball/keyball61/rules.mk
+++ keyboards/keyball/keyball61/rules.mk
@@ -31,9 +31,7 @@ SPLIT_KEYBOARD = yes
 # To support trackball.
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = custom
-#SRC += trackball.c              # Trackball application interface
 SRC += drivers/pmw3360/pmw3360.c
-#SRC += drivers/pmw3360/pmw3360_pointing_device_driver.c
 QUANTUM_LIB_SRC += spi_master.c # Optical sensor use SPI to communicate
 
 # To support OLED
