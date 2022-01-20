98fe1111d0817fbe4e57e634c78a784d7b33c013
MURAOKA Taro <koron.kaoriya@gmail.com>
2022-01-20 22:48:39 +0900

    keyball61: fix

--- keyboards/keyball/keyball61/keymaps/test/keymap.c
+++ keyboards/keyball/keyball61/keymaps/test/keymap.c
@@ -31,12 +31,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LGUI  , KC_APP   , KC_HOME  , KC_END   , KC_LALT  , KC_SPC   , KC_TAB   ,      KC_BSPC  , KC_ENT   , KC_RALT  , KC_PGUP  , KC_PGDN  , KC_BSLS  , KC_RGUI
   ),
 #else
+  // keymap for development
   [0] = LAYOUT_universal(
-    KC_ESC   , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                            KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , CPI_RST  ,
-    KC_GRV   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , CPI_I100 , CPI_I1K  , KC_O     , KC_P     , KC_EQL   ,
-    KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , CPI_D100 , CPI_D1K  , KC_L     , KC_SCLN  , KC_QUOT  ,
-    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_LBRC  ,      KC_RBRC  , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT  ,
-    KC_LGUI  , KC_APP   , KC_HOME  , KC_END   , KC_LALT  , KC_SPC   , KC_TAB   ,     KC_MS_BTN2,KC_MS_BTN1, KC_RALT  , KC_PGUP  , KC_PGDN  , KC_BSLS  , KC_RGUI
+    KC_ESC   , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                            KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_MINS  ,
+    KC_GRV   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_EQL   ,
+    KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
+    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_LBRC  ,      KC_RBRC  , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_ENT   ,
+    KC_LGUI  , KC_APP   , KC_HOME  , KC_END   , KC_LALT  ,KC_MS_BTN1, MO(1)    ,      MO(1)    , KC_SPC   , KC_RALT  , KC_PGUP  , KC_PGDN  , KC_BSLS  , KC_RGUI
+  ),
+
+  [1] = LAYOUT_universal(
+    RGB_TOG  , _______  , _______  , _______  , _______  , _______  ,                            RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN , RGB_M_K  ,
+    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , _______  ,                            RGB_M_K  , RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW , _______  ,
+    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , _______  ,                            CPI_D100 , CPI_I100 , CPI_I1K  , CPI_D1K  , _______  , CPI_RST  ,
+    _______  , _______  , _______  , _______  , _______  , _______  , EEP_RST  ,      EEP_RST  , _______  , _______  , _______  , _______  , _______  , _______  ,
+    RESET    , _______  , _______  , _______  , _______  , _______  , _______  ,      _______  , _______  , _______  , _______  , _______  , _______  , RESET
   ),
 #endif
 };
