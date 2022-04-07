f3fade1abbbf1717226d0365c8d478d77c260eed
MURAOKA Taro <koron.kaoriya@gmail.com>
2022-04-07 21:32:41 +0900

    crkbd/test: handiness by matrix grid

--- keyboards/crkbd/keymaps/test/config.h
+++ keyboards/crkbd/keymaps/test/config.h
@@ -24,9 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
-#define MASTER_LEFT
-// #define MASTER_RIGHT
-// #define EE_HANDS
+#define SPLIT_HAND_MATRIX_GRID  E6, F6
 
 //#define TAPPING_FORCE_HOLD
 //#define TAPPING_TERM 100
