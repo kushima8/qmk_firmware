a18a4ea4dabf589f1b93bcd9567bf80894991b6d
kushima8 <58157342+kushima8@users.noreply.github.com>
2025-07-05 00:59:31 +0900

    dix & add reex_rp2040

--- keyboards/reex_rp2040/reex56/config.h
+++ keyboards/reex_rp2040/reex56/config.h
@@ -5,29 +5,18 @@
 
 // USB Device descriptor parameters
 //#define VENDOR_ID           0x3938
-//#define PRODUCT_ID          0x5236
+//#define PRODUCT_ID          0x3536
 //#define DEVICE_VER          0x0001
 //#define MANUFACTURER        kushima8
-//#define PRODUCT             Reex67mod
+//#define PRODUCT             Reex56
 
 /* key matrix size */
-#define MATRIX_ROWS         (6 * 2)  // split keyboard
+#define MATRIX_ROWS         (5 * 2)  // split keyboard
 #define MATRIX_COLS         (4 * 2)  // duplex matrix
-#define MATRIX_ROW_PINS     { D4, C6, D7, E6, B4, F6 }
-#define MATRIX_COL_PINS     { B5, F4, F5, B6 }
+#define MATRIX_ROW_PINS     { GP4, GP5, GP6, GP7, GP8 }
+#define MATRIX_COL_PINS     { GP9, GP29, GP28, GP10 }
 #define MATRIX_MASKED
 #define DEBOUNCE            5
-
-#define ENCODERS_PAD_A { B2 }
-#define ENCODERS_PAD_B { B3 }
-#define ENCODER_RESOLUTION 4
-//#define ENCODERS_PAD_A_RIGHT { B3 }
-//#define ENCODERS_PAD_B_RIGHT { B2 }
-//#define ENCODER_RESOLUTIONS_RIGHT { 4 }
-#define ENCODER_MAP_KEY_DELAY 10
-
-#define DIP_SWITCH_PINS { B1 }
-//#define DIP_SWITCH_PINS_RIGHT { F7 }
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 3
@@ -38,10 +27,12 @@
 #define DYNAMIC_KEYMAP_LAYER_COUNT 4
 
 // Split parameters
-#define SOFT_SERIAL_PIN         D2
-#define SPLIT_HAND_MATRIX_GRID  B4, B6
+#define SERIAL_USART_TX_PIN GP1
+#define SPLIT_HAND_MATRIX_GRID GP8, GP10
+#define SPLIT_HAND_MATRIX_GRID_LOW_IS_LEFT
 #define SPLIT_USB_DETECT
-//#define SPLIT_USB_TIMEOUT       500
+#define SPLIT_USB_TIMEOUT 4500
+#define SPLIT_USB_TIMEOUT_POLL 25
 #ifdef OLED_ENABLE
 #    define SPLIT_OLED_ENABLE
 #endif
@@ -54,10 +45,11 @@
 #define SPLIT_TRANSACTION_IDS_KB REEX_GET_INFO, REEX_GET_MOTION, REEX_SET_CPI
 
 // RGB LED settings
-#define WS2812_DI_PIN       D3
+#define WS2812_DI_PIN GP0
+#define WS2812_PIO_USE_PIO1
 #ifdef RGBLIGHT_ENABLE
-#    define RGBLED_NUM      75
-#    define RGBLED_SPLIT    { 33, 42 }
+#    define RGBLED_NUM    64
+#    define RGBLED_SPLIT    { 32, 32 }
 #    ifndef RGBLIGHT_LIMIT_VAL
 #        define RGBLIGHT_LIMIT_VAL  100 // limitated for power consumption
 #    endif
@@ -72,7 +64,7 @@
 #    endif
 #endif
 #ifdef RGB_MATRIX_ENABLE
-#    define RGB_MATRIX_SPLIT    { 33, 42 }
+#    define RGB_MATRIX_SPLIT    { 32, 32 }
 #endif
 
 #ifndef OLED_FONT_H
@@ -90,17 +82,32 @@
 #undef LOCKING_RESYNC_ENABLE
 
 #ifdef RGBLIGHT_ENABLE
-//#    define RGBLIGHT_EFFECT_BREATHING
-//#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-//#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-//#    define RGBLIGHT_MODE_SNAKE
-//#    define RGBLIGHT_MODE_KNIGHT
-//#    define RGBLIGHT_MODE_CHRISTMAS
-//#    define RGBLIGHT_MODE_STATIC_GRADIENT
-//#    define RGBLIGHT_EFFECT_RGB_TEST
-//#    define RGBLIGHT_MODE_ALTERNATING
-//#    define RGBLIGHT_MODE_TWINKLE
+#    define RGBLIGHT_EFFECT_BREATHING
+#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#    define RGBLIGHT_MODE_SNAKE
+#    define RGBLIGHT_MODE_KNIGHT
+#    define RGBLIGHT_MODE_CHRISTMAS
+#    define RGBLIGHT_MODE_STATIC_GRADIENT
+#    define RGBLIGHT_EFFECT_RGB_TEST
+#    define RGBLIGHT_MODE_ALTERNATING
+#    define RGBLIGHT_MODE_TWINKLE
 #endif
 
 #define TAP_CODE_DELAY 5
 
+#define SPI_DRIVER SPID0
+#define SPI_SCK_PIN GP18
+#define SPI_MISO_PIN GP20
+#define SPI_MOSI_PIN GP19
+#define PMW3360_NCS_PINS {GP26,GP27}
+
+//#define REEX_PMW3360_UPLOAD_SROM_ID 0x04
+#define REEX_PMW3360_UPLOAD_SROM_ID 0x81
+
+/* define RP2040 boot	 */
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
+
+//#define SELECT_SOFT_SERIAL_SPEED 3
+#define SERIAL_USART_SPEED 86400
